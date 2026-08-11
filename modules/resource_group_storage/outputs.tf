bab7d0b42000b9f75afa147bf15ec535beb6bf7f
1ntact <verbaivan9@gmail.com>
2026-08-11 20:12:11 +0200

    Add module variables and outputs

---
+++ modules/resource_group_storage/outputs.tf
@@ -0,0 +1,19 @@
+output "resource_group_id" {
+  description = "ID of the created resource group"
+  value       = azurerm_resource_group.main.id
+}
+
+output "resource_group_name" {
+  description = "Name of the created resource group"
+  value       = azurerm_resource_group.main.name
+}
+
+output "storage_account_id" {
+  description = "ID of the created storage account"
+  value       = azurerm_storage_account.main.id
+}
+
+output "storage_account_name" {
+  description = "Name of the created storage account"
+  value       = azurerm_storage_account.main.name
+}
