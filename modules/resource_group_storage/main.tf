e90660749f2cf1fc76b516c4eeecef43bd7ed901
1ntact <verbaivan9@gmail.com>
2026-08-11 21:28:27 +0200

    fix: define actual azurerm resources in local module instead of self-referencing module call

--- modules/resource_group_storage/main.tf
+++ modules/resource_group_storage/main.tf
@@ -1,23 +1,12 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-  }
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
-provider "azurerm" {
-  features {}
-}
-
-module "resource_group_storage" {
-  source  = "app.terraform.io/1ntact/resource_group_storage/azurerm"
-  version = "1.0.1"
-
-  resource_group_name      = "my-resource-group"
-  location                 = "West Europe"
-  storage_account_name     = "intactstorage20260811"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_storage_account" "main" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
 }
